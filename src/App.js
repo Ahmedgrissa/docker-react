c0e376a1564901133922b93f9aa22f2b44ef1559
Ahmedgrissa <grissahmed@gmail.com>
2020-01-23 23:13:02 +0100

    add travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> BACK OFF TO FUCK.
+          Edit <code>src/App.js</code>qjsdkqsjd
         </p>
         <a
           className="App-link"
